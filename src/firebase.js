cf828ea28a8bfd521dd0bb2bed4b055474cb0e0b
Alonso Hernandez <ahernandez@cpaglobal.com>
2020-12-22 16:07:40 -0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD9fsS1fOi5RqJYfoAAXh-u7gXOluxMH9A",
+  authDomain: "clone-953d9.firebaseapp.com",
+  projectId: "clone-953d9",
+  storageBucket: "clone-953d9.appspot.com",
+  databaseURL: "https://clone-953d9-default-rtdb.firebaseio.com/",
+  messagingSenderId: "798157488545",
+  appId: "1:798157488545:web:fd61fc01562903746fcbe0",
+  measurementId: "G-NB0K2EX4DN"
+};
