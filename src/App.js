6f7e9224b35331847698267813879ce91b118b3f
Alonso Hernandez <ahernandez@cpaglobal.com>
2020-12-22 21:58:36 -0600

    finished header/adding banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>let's build something!</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
-  return <div classname="header">
-      
-  </div>;
+  return (
+    <div className="header">
+      <img className="header__logo" src="../logo.png" alt="logo" />
+      <div className="header__search">
+        <input className="header_searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionFirstLine">Hello Guest</span>
+          <span className="header__optionSecondLine">Sign in</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionFirstLine">Return</span>
+          <span className="header__optionSecondLine">& Orders</span>
+        </div>
+
+        <div className="header__option">
+          <span className="header__optionFirstLine">Your</span>
+          <span className="header__optionSecondLine">Prime</span>
+        </div>
+
+        <div className="header__basketOption">
+          <ShoppingBasketIcon />
+          <span className="header_itemsCheckOut">1</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
